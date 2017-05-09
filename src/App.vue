bf8413cfc6f50e2ca49473a123eefa6b96fd8cad
LoicMonard <bobbycarotte@outlook.fr>
2017-05-09 23:15:10 +0200

    Add overlay on playlist

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,15 @@
         <div class="xxxplaylistContainer">
           <div id="xxxsinglePlaylist" v-for="playlist in playlists">
             <img v-bind:src="playlist.img">
+            <div id="xxxplaylistOverlay">
+              <img id="xxxcheckIcon" src="./assets/Icones/check.png" alt="Check icon">
+            </div>
           </div>
         </div>
         <div id="xxxgradientBack">
         </div>
       </div>
+
       <div class="xxxmyFriendsPlaylists">
         <span id="xxxmenuTitle">Mes playlists</span>
         <div class="xxxplaylistContainer">
@@ -46,19 +50,19 @@ export default {
   data () {
     return {
       playlists: [
-        {name: '1', img: 'https://4.bp.blogspot.com/-Qls0bKPYaBY/WBdlpnE1vBI/AAAAAAAAid0/KI3fl-7x270i2Ywcr_2DmimhJp2PoljEgCLcB/s1600/Playlist-91coverart.jpg'},
-        {name: '1', img: 'https://4.bp.blogspot.com/-Qls0bKPYaBY/WBdlpnE1vBI/AAAAAAAAid0/KI3fl-7x270i2Ywcr_2DmimhJp2PoljEgCLcB/s1600/Playlist-91coverart.jpg'},  
-        {name: '1', img: 'https://4.bp.blogspot.com/-Qls0bKPYaBY/WBdlpnE1vBI/AAAAAAAAid0/KI3fl-7x270i2Ywcr_2DmimhJp2PoljEgCLcB/s1600/Playlist-91coverart.jpg'}, 
-        {name: '1', img: 'https://4.bp.blogspot.com/-Qls0bKPYaBY/WBdlpnE1vBI/AAAAAAAAid0/KI3fl-7x270i2Ywcr_2DmimhJp2PoljEgCLcB/s1600/Playlist-91coverart.jpg'}, 
-        {name: '1', img: 'https://4.bp.blogspot.com/-Qls0bKPYaBY/WBdlpnE1vBI/AAAAAAAAid0/KI3fl-7x270i2Ywcr_2DmimhJp2PoljEgCLcB/s1600/Playlist-91coverart.jpg'}, 
-        {name: '1', img: 'https://4.bp.blogspot.com/-Qls0bKPYaBY/WBdlpnE1vBI/AAAAAAAAid0/KI3fl-7x270i2Ywcr_2DmimhJp2PoljEgCLcB/s1600/Playlist-91coverart.jpg'}, 
-        {name: '1', img: 'https://4.bp.blogspot.com/-Qls0bKPYaBY/WBdlpnE1vBI/AAAAAAAAid0/KI3fl-7x270i2Ywcr_2DmimhJp2PoljEgCLcB/s1600/Playlist-91coverart.jpg'}, 
-        {name: '1', img: 'https://4.bp.blogspot.com/-Qls0bKPYaBY/WBdlpnE1vBI/AAAAAAAAid0/KI3fl-7x270i2Ywcr_2DmimhJp2PoljEgCLcB/s1600/Playlist-91coverart.jpg'}, 
-        {name: '1', img: 'https://4.bp.blogspot.com/-Qls0bKPYaBY/WBdlpnE1vBI/AAAAAAAAid0/KI3fl-7x270i2Ywcr_2DmimhJp2PoljEgCLcB/s1600/Playlist-91coverart.jpg'}, 
-        {name: '1', img: 'https://4.bp.blogspot.com/-Qls0bKPYaBY/WBdlpnE1vBI/AAAAAAAAid0/KI3fl-7x270i2Ywcr_2DmimhJp2PoljEgCLcB/s1600/Playlist-91coverart.jpg'},
-        {name: '1', img: 'https://4.bp.blogspot.com/-Qls0bKPYaBY/WBdlpnE1vBI/AAAAAAAAid0/KI3fl-7x270i2Ywcr_2DmimhJp2PoljEgCLcB/s1600/Playlist-91coverart.jpg'},
-        {name: '1', img: 'https://4.bp.blogspot.com/-Qls0bKPYaBY/WBdlpnE1vBI/AAAAAAAAid0/KI3fl-7x270i2Ywcr_2DmimhJp2PoljEgCLcB/s1600/Playlist-91coverart.jpg'},
-        {name: '1', img: 'https://4.bp.blogspot.com/-Qls0bKPYaBY/WBdlpnE1vBI/AAAAAAAAid0/KI3fl-7x270i2Ywcr_2DmimhJp2PoljEgCLcB/s1600/Playlist-91coverart.jpg'}  
+        {name: '1', img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ8oSldZDiZkQkxqhUaixRmbCHUHieUf5Tvn25WqyxuO6A97ssJ'},
+        {name: '1', img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTgz6hZmEOKhFtTyksQ8YO-zYyJKIxgslNsJc0kOg1nZXLn9LTu'},  
+        {name: '1', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuix2_4M4wJW5WPulr4Jiv3m6H6nJwXL-4FZAF0ixMijcr-a3U'}, 
+        {name: '1', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvrjhfN_GWaK4E1dOrsQ4bTFpOHWTpT2UqCO3tixJQyph1r2rl'}, 
+        {name: '1', img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQxVolTA5obssR6TBbdXs-DqgPFddWZCh2KSSbXBFUatj3A2gf_6Q'}, 
+        {name: '1', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDOOB9RK2hfXdA9pd3ivlHfTILdQU3AjBMOo6RNI8mgQ0N0sHi'}, 
+        {name: '1', img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT-b_KEtBRBuPBIsQCkdxpF7Xy6V0-BSY71zeoDwyB_rYgZOo1x'}, 
+        {name: '1', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHRNtaLNKcGsxW0N7PDXIJVuI92ywdB3SVAQVkO_s4OcoXNT2a'}, 
+        {name: '1', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe9NcDacC3OZjhN02x5kNL-Qevv-3fHhBIU7_3bowaQTa_Ia28'}, 
+        {name: '1', img: 'http://loudwire.com/files/2011/11/Nirvana-Kitten-Album-Cover.jpg'},
+        {name: '1', img: 'https://s-media-cache-ak0.pinimg.com/originals/50/f4/d5/50f4d510a9c309d587c3108606ae8ac1.jpg'},
+        {name: '1', img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRhh4LKCVmSZS3GsdKp065AnZjk6pwRpSyP8RjA3rokYDTZ4Bly'},
+        {name: '1', img: 'https://s-media-cache-ak0.pinimg.com/736x/28/3e/8d/283e8d5f7263e56bee957a9ddb3f336d--cd-design-album-cover-design.jpg'}  
       ]
     }
   },
